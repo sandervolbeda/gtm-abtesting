1a1fbf25d9c50cfca265825f66867d54e91a533b
Sander Volbeda <hallo@sandervolbeda.com>
2023-09-14 11:00:09 +0200

    GSC fix + GTM fix

--- site/javascripts/extra.js
+++ site/javascripts/extra.js
@@ -1,10 +1,17 @@
-// (function () {
-//     var gtmContainerId = 'GTM-525ZBCP9'; // Replace with your GTM container ID
-//     var script = document.createElement('script');
-//     script.src = 'https://www.googletagmanager.com/gtm.js?id=' + gtmContainerId;
-//     script.async = true;
-//     document.head.appendChild(script);
-// })();
+(function () {
+    // Create a meta element
+    var metaTag = document.createElement('meta');
+
+    // Set the attributes for the meta element
+    metaTag.setAttribute('name', 'google-site-verification');
+    metaTag.setAttribute('content', '_IRB9D2-ldvb6stuu-YXM7dpraqRAY2eAnPcrwaA0nE');
+
+    // Get the head element of your HTML document
+    var head = document.head || document.getElementsByTagName('head')[0];
+
+    // Append the meta element to the head
+    head.appendChild(metaTag);
+})();
 
 (function () {
 // Create the Google Tag Manager script element
