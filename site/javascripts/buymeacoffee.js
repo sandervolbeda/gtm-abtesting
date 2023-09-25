2529e7322cca39a151ba8688fc9072fa3ae9e6de
Sander Volbeda <hallo@sandervolbeda.com>
2023-09-25 15:39:37 +0200

    Bug fix deployement

--- site/javascripts/buymeacoffee.js
+++ site/javascripts/buymeacoffee.js
@@ -1,17 +1,22 @@
-// Create a script element
-var scriptElement = document.createElement("script");
+(function () {
+    // Create a script element for the Buy Me a Coffee widget
+    var bmcsScript = document.createElement('script');
 
-// Set attributes for the script
-scriptElement.setAttribute("data-name", "BMC-Widget");
-scriptElement.setAttribute("data-cfasync", "false");
-scriptElement.setAttribute("src", "https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js");
-scriptElement.setAttribute("data-id", "sandervolbeda");
-scriptElement.setAttribute("data-description", "Support me on Buy me a coffee!");
-scriptElement.setAttribute("data-message", "Support this documentation");
-scriptElement.setAttribute("data-color", "#FF813F");
-scriptElement.setAttribute("data-position", "Right");
-scriptElement.setAttribute("data-x_margin", "18");
-scriptElement.setAttribute("data-y_margin", "18");
+    // Set the attributes for the script
+    bmcsScript.setAttribute('data-name', 'BMC-Widget');
+    bmcsScript.setAttribute('data-cfasync', 'false');
+    bmcsScript.setAttribute('src', 'https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js');
+    bmcsScript.setAttribute('data-id', 'sandervolbeda');
+    bmcsScript.setAttribute('data-description', 'Support me on Buy me a coffee!');
+    bmcsScript.setAttribute('data-message', 'Support this documentation');
+    bmcsScript.setAttribute('data-color', '#FF813F');
+    bmcsScript.setAttribute('data-position', 'Right');
+    bmcsScript.setAttribute('data-x_margin', '18');
+    bmcsScript.setAttribute('data-y_margin', '18');
 
-// Append the script to the document's body
-document.body.appendChild(scriptElement);
+    // Get the footer element of your HTML document
+    var footer = document.querySelector('footer');
+
+    // Append the Buy Me a Coffee script element to the footer
+    footer.appendChild(bmcsScript);
+})();
